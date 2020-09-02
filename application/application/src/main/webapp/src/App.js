a610760a65e25a400dc37e5e25adfd256aff9d5f
mertsaadet <47190467+mertsaadet@users.noreply.github.com>
2020-09-03 01:04:47 +0300

    Added Update button and functionality to Frontend

--- application/application/src/main/webapp/src/App.js
+++ application/application/src/main/webapp/src/App.js
@@ -5,6 +5,7 @@ import {Button} from "@material-ui/core";
 import PlusIcon from '@material-ui/icons/Add';
 import AddButtonDialog from "./components/dialog/AddButtonDialog";
     import DeleteEventDialog from "./components/dialog/DeleteEventDialog";
+    import UpdateEventDialog from "./components/dialog/UpdateEventDialog";
 function App() {
 
     const tableColumns = [
@@ -46,11 +47,16 @@ function App() {
         {id: "quota", label: "Quota", type: "number"}
 
     ];
-    const deleteEventDialogField = [{id: "eventName", label:"Type the event you want to delete!", type: "text" }]
+    const deleteEventDialogField = [{id: "eventName", label:"Type the event you want to delete!", type: "text" }];
+    const updateEventDialogField = [{id: "eventName", label:"Type the event you want to edit!", type: "text" },
+        {id: "attribute", label:"Type the attribute you want to change", type: "text"},
+        {id: "value", label:"Type the value!", type: "text"}]
+
     const [tableRows, updateTableRows] = useState([]);
 
     const [open, setOpen] = React.useState(false);
     const [open2, setOpen2] = React.useState(false);
+    const [open3, setOpen3] = React.useState(false);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -58,11 +64,17 @@ function App() {
     const handleClickOpen2 = () => {
         setOpen2(true);
     };
+    const handleClickOpen3 = () => {
+        setOpen3(true);
+    };
     const handleClose = () => {
         setOpen(false);
     };
     const handleClose2 = () => {
         setOpen2(false);
+    };
+    const handleClose3 = () => {
+        setOpen3(false);
     };
 
     useEffect(() => {
@@ -84,7 +96,34 @@ function App() {
         axios.delete("/events/"+inputData.eventName).then(res => {console.log(res)});
         handleClose2();
     }
+    const onEventUpdate = (inputData) => {
 
+        axios.get("/events/"+inputData.eventName).then(res => {
+            if(inputData.attribute === "longitude"){
+                res.data.longitude = inputData.value;
+                axios.put("/events/"+inputData.eventName,res.data);
+            }
+            else if(inputData.attribute === "latitude"){
+                res.data.latitude = inputData.value;
+                axios.put("/events/"+inputData.eventName,res.data);
+            }
+            else if(inputData.attribute === "eventStartDate"){
+                res.data.eventStartDate = inputData.value;
+                axios.put("/events/"+inputData.eventName,res.data);
+            }
+            else if(inputData.attribute === "eventEndDate"){
+                res.data.eventEndDate = inputData.value;
+                axios.put("/events/"+inputData.eventName,res.data);
+            }
+            else if(inputData.attribute === "quota"){
+                res.data.quota = inputData.value;
+                axios.put("/events/"+inputData.eventName,res.data);
+            }
+
+
+            console.log(res)});
+
+    }
 
 
   return (
@@ -99,8 +138,15 @@ function App() {
                 color="secondary"
                 style={{float:"right"}}
                 onClick={handleClickOpen2}
-                startIcon={<PlusIcon/>}>Delete Event
+                >Delete Event
         </Button>
+        <Button variant="contained"
+                color="primary"
+                style={{float:"right"}}
+                onClick={handleClickOpen3}
+                >Update Event
+        </Button>
+        <UpdateEventDialog onSubmit={onEventUpdate} fields={updateEventDialogField} open={open3} handleClose={handleClose3}/>
         <DeleteEventDialog onSubmit={onEventDelete} fields={deleteEventDialogField} open={open2} handleClose={handleClose2}/>
         <AddButtonDialog onSubmit={onEventAdd} fields={addEventDialogFields} open={open} handleClose={handleClose}/>
       <PaginationTable columns={tableColumns} rows={tableRows} />
